08901b2f7e0ec5707d4ff4ad0f905fe2506291c2
tvminh <tvminh1701@gmail.com>
2024-04-15 14:10:37 +0700

    Apply disaster recovery strategy with CloudFormation StackSet

---
+++ update-set.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ex
+
+
+aws s3 sync . s3://tvm-cf-templates --exclude ".git/*" --profile minh
+aws s3 sync . s3://tvm-cf-templates-tokyo --exclude ".git/*" --profile minh
+
+aws cloudformation update-stack --stack-name stack-set \
+      --template-body file://stackset.yml \
+      --profile minh \
+      --capabilities CAPABILITY_NAMED_IAM 
+
